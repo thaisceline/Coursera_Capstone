c08b0cfb9b6fe42a0cf877192114cebb7b4c33ce
thaisceline <thceline@gmail.com>
2019-07-31 11:03:08 +0100

    notebook for capstone project

---
+++ Coursera-capstone.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# ## Applied Data Science - Coursera
+# ### Capstone Project
+# This notebook will be used for the assignment of the course
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
